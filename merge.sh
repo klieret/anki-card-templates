2409bd0254aeabe7077b5fc82c0b6235b7c2b139
klieret <klieret@users.noreply.github.com>
2016-11-11 19:22:11 +0100

    Script to merge all prior repositories.

---
+++ merge.sh
@@ -0,0 +1,29 @@
+set -o xtrace # Print every command you run.
+
+set -e
+
+git init
+
+git add "${0}"
+git commit -m "Script to merge all prior repositories."
+
+include(){
+    local repo_path=${1}
+    local name=${2}
+    local subdir=${3}
+    git remote add "${name}" "$repo_path"
+    git fetch "${name}"
+    git branch "${name}" "${name}/master" 
+    git checkout "${name}"
+    mkdir -p "${subdir}"
+    git ls-tree -z --name-only HEAD | xargs -0 -I {} git mv {} "${subdir}"
+    git commit -m "Moved files from ${name} to subdir"
+    git checkout master
+    git merge --allow-unrelated-histories ${name} -m "Merge files from ${name}."
+    git remote remove ${name}
+    git branch -d ${name}
+}
+
+include "../kanji/keyword" "keyword_kanji" "kanji/keyword_kanji"
+include "../readings" "readings" "readings"
+include "../vocab" "vocab" "vocab"
