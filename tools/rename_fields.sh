9f2fd8dcfcf6a4bcf3d9f849cf257f31a71636a9
klieret <klieret@users.noreply.github.com>
2017-04-04 09:25:24 +0200

    do not try to rename css class names

--- tools/rename_fields.sh
+++ tools/rename_fields.sh
@@ -7,4 +7,6 @@ lower_replacement=$(echo "$replacement" | tr '[:upper:]' '[:lower:]')
 sed -i "s/{{${search}}}/{{${replacement}}}/g" *.html
 sed -i "s/{{#${search}}}/{{#${replacement}}}/g" *.html
 sed -i "s/{{\/${search}}}/{{\/${replacement}}}/g" *.html
-sed -i "s/${lower_search}/${lower_replacement}/g" *.html *.css
+# sed -i "s/${lower_search}/${lower_replacement}/g" *.html *.css
+# was intended to rename css class names etc, but will cause more trouble
+# than saves time
