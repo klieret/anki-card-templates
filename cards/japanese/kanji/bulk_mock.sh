6679d50a4e1712a754eca865ae2ff95ff4e7d864
klieret <klieret@users.noreply.github.com>
2017-04-17 09:41:34 +0200

    fix naming of generated html

--- cards/japanese/kanji/bulk_mock.sh
+++ cards/japanese/kanji/bulk_mock.sh
@@ -16,11 +16,15 @@ cd "${source_path}"
 
 echo "Note: this script only works if there are no blank spaces in your paths."
 
+branch=$(git rev-parse --abbrev-ref HEAD)
+
 for field_file in ${mock_field_folder}/*.py; do  
     for template in ${template_folder}/*.html; do
         template_basename=$(basename ${template})
+        template_basename=${template_basename%.*}
         field_basename=$(basename ${field_file})
-        echo $field_basename $template_basename
-        python3 "template-tester.py" --style "${template_folder}/style.css" --output "${output_path}/${field_basename}_${template_basename}" "${template}" "${field_file}"
+        field_basename=${field_basename%.*}
+        echo "branch" $branch "field" $field_basename "template" $template_basename
+        python3 "template-tester.py" --style "${template_folder}/style.css" --output "${output_path}/${field_basename}_${branch}_${template_basename}.html" "${template}" "${field_file}"
     done
 done
