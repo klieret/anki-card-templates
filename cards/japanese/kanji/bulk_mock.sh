02d4a3045c444c5593d598cc08912ab5a74ff9da
klieret <klieret@users.noreply.github.com>
2018-10-30 19:07:20 +0900

    make bulk_mock run again

--- cards/japanese/kanji/bulk_mock.sh
+++ cards/japanese/kanji/bulk_mock.sh
@@ -6,15 +6,12 @@
 set -e  # abort if errors
 echo "Note: this script only works if there are no blank spaces in your paths."
 
-mock_field_folder="$HOME/Documents/prog/japan/anki/cards/cards/japanese/kanji/mock_fields"
-template_folders="$HOME/Documents/prog/japan/anki/cards/cards/japanese/kanji/new_design $HOME/Documents/prog/japan/anki/cards/cards/japanese/kanji/legacy_design"
-source_path="$HOME/Documents/prog/japan/anki/card-tester/"
-output_path="$HOME/Documents/prog/readme_files/anki-card-templates/cards/japanese/kanji/html_rendered"
+mock_field_folder="mock_fields"
+template_folders="new_design legacy_design"
+source_path="$HOME/Documents/git_sync/anki-template-tester"
+output_path="html_rendered"
 
-# python can only be called with source files in a subdirectory, 
-# so we have to change dirs.
-cd "${source_path}"
-
+mkdir -p "${output_path}"
 
 echo "To include the previews, include the following lines in the readme:"
 echo "|design|git-branch|field-file|template|link|"
@@ -24,7 +21,7 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 
 for template_folder in ${template_folders}; do 
     template_folder_basename=$(basename ${template_folder})
-    for field_file in ${mock_field_folder}/*.py; do
+    for field_file in ${mock_field_folder}/*.txt; do
         for template in ${template_folder}/*.html; do
             template_basename=$(basename ${template})
             template_basename=${template_basename%.*}
@@ -33,7 +30,7 @@ for template_folder in ${template_folders}; do
             output_filename="${branch}_${template_folder_basename}_${field_basename}_${template_basename}.html"
             output_github_path="http://htmlpreview.github.com/?https://github.com/klieret/readme-files/blob/master/anki-card-templates/cards/japanese/kanji/html_rendered/${output_filename}"
             echo "|\`\`\`${template_folder_basename}\`\`\`|\`\`\`${branch}\`\`\`|\`\`\`${field_basename}\`\`\`|\`\`\`${template_basename}\`\`\`|[html preview](${output_github_path})"
-            python3 "template-tester.py" --style "${template_folder}/style.css" --output "${output_path}/${output_filename}" "${template}" "${field_file}"
+            python3 "${source_path}/previewtemplate.py" --style "${template_folder}/style.css" --output "${output_path}/${output_filename}" "${template}" "${field_file}"
         done
     done
 done
